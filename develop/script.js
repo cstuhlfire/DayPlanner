259d9b2d2cb13ce72e02695f21e83eb83baacce6
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-03-25 04:34:41 -0500

    add/ Added event listeners for save buttons

--- develop/script.js
+++ develop/script.js
@@ -1,79 +1,97 @@
 // DayPlanner
 
+// Storage array of objects
+let storeObject = {
+  time: "",
+  task: "",
+};
+
+let objectArray = [];
+
 // Query selectors
 let todayEl = document.querySelector("#currentDay");
+let saveBtnEl = document.querySelectorAll(".saveBtn");
 let tableRowsEl = document.querySelectorAll("tr");
+let textsEl = document.querySelectorAll("textarea");
 
 // Function calls
 main();
 
-
 // Function definitions
 function main() {
-    setDate();
-    setTableColors();
+  setDate();
+  setTableColors();
 }
 
 function setDate() {
-    let today = moment().format("dddd, MMM Do");
-    console.log(today);
-    todayEl.textContent = today;
-    
-    return;
+    // Use moment to set date.
+  let today = moment().format("dddd, MMM Do");
+
+  todayEl.textContent = today;
+  return;
 }
 
 function convertTime(numericPart, moridiem) {
-    let militaryTime = 0;
+  let militaryTime = 0;
 
-    if(numericPart === 12 && moridiem === "AM") {
-        militaryTime = 0;
-        
-    } else if (moridiem === "AM" || (numericPart === 12 && moridiem === "PM")) {
-        militaryTime = numericPart * 100;
-        
-    } else if (moridiem === "PM") {
-        militaryTime = (numericPart + 12) * 100;
-    }
-    
-    return parseInt(militaryTime);
+  if (numericPart === 12 && moridiem === "AM") {
+    militaryTime = 0;
+  } else if (moridiem === "AM" || (numericPart === 12 && moridiem === "PM")) {
+    militaryTime = numericPart * 100;
+  } else if (moridiem === "PM") {
+    militaryTime = (numericPart + 12) * 100;
+  }
+
+  return parseInt(militaryTime);
 }
 
 function parseTime(timeString) {
-    let split = [];
-    let numericPart = 0;
-    let moridiem = "";
-    let militaryTime = 0;
+  let split = [];
+  let numericPart = 0;
+  let moridiem = ""; // am/pm
+  let militaryTime = 0;
 
-    // Parse number and am/pm part of time
-    split = timeString.split(" ");
-    
-    numericPart = parseInt(split[0]);
-    moridiem = split[1].toUpperCase();
-    militaryTime = convertTime(numericPart, moridiem);
+  // Parse number and am/pm part of time
+  split = timeString.split(" ");
 
-    return militaryTime;
+  numericPart = parseInt(split[0]);
+  moridiem = split[1].toUpperCase();
+  militaryTime = convertTime(numericPart, moridiem);
+
+  return militaryTime;
 }
 
 function setTableColors() {
-    // Get current hour
-    let currentTime = 0;
-    let calTime = 0;
+  // Get current hour
+  let currentTime = 0;
+  let calTime = 0;
 
-    console.log(currentTime);
-    currentTime = parseTime(moment().format("h a"));
+  currentTime = parseTime(moment().format("h a"));
 
-    for (let i = 0; i < tableRowsEl.length; i++) {
-        calTime = parseTime(tableRowsEl[i].children[0].textContent);
-        console.log(calTime + " "+ currentTime);
+  for (let i = 0; i < tableRowsEl.length; i++) {
+    calTime = parseTime(tableRowsEl[i].children[0].textContent);
 
-        if (calTime < currentTime) {
-            tableRowsEl[i].classList.add("past");
-        } else if (calTime > currentTime) {
-            tableRowsEl[i].classList.add("future");
-        } else {
-            tableRowsEl[i].classList.add("present");
-        }    
+    if (calTime < currentTime) {
+      tableRowsEl[i].classList.add("past");
+    } else if (calTime > currentTime) {
+      tableRowsEl[i].classList.add("future");
+    } else {
+      tableRowsEl[i].classList.add("present");
     }
-    
-    return;
+  }
+
+  return;
+}
+
+function saveLocalStorage(x, y) {
+    console.log(x+" "+y);
+}
+
+// Event listeners
+for (let i = 0; i < saveBtnEl.length; i++) {
+    const element = saveBtnEl[i];
+
+    element.addEventListener("click", function () {
+        saveLocalStorage(tableRowsEl[i].children[0].textContent, textsEl[i].value);
+    })
 }
